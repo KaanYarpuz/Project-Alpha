5fb67a9dd165add7eaca82493fc0881c69afc7ee
EmGnome <leonvhr@gmail.com>
2026-02-24 14:45:33 +0100

    Made monster dih public

--- Project-Alpha/Monster.cs
+++ Project-Alpha/Monster.cs
@@ -1,17 +1,17 @@
-class Monster
+public class Monster
 {
     // fields
-    int ID;
-    string Name;
-    int Attack;
-    int Health;
+    public int ID;
+    public string Name;
+    public int Attack;
+    public int Health;
 
     // Constructor
     public Monster (int C_id, string C_Name, int C_Attack, int C_Health)
     {
-        this.ID = C_id;
-        this.Name = C_Name;
-        this.Attack = C_Attack;
-        this.Health = C_Health;
+        ID = C_id;
+        Name = C_Name;
+        Attack = C_Attack;
+        Health = C_Health;
     }
 } 
