9b9bed7f301d8fd72ee3c5877ebe3b7a84bdf747
EmGnome <leonvhr@gmail.com>
2026-02-24 12:11:40 +0100

    Made monster class like a goodboy

--- Project-Alpha/Monster.cs
+++ Project-Alpha/Monster.cs
@@ -1,10 +1,17 @@
-using System;
-using System.Collections.Generic;
-
-namespace Project_Alpha{
-
-public class Monster
+class Monster
 {
-   
-}
-}
+    // fields
+    int ID;
+    string name;
+    int Attack;
+    int Health;
+
+    // Constructor
+    public Monster (int C_id, string C_Name, int C_Attack, int C_Health)
+    {
+        this.ID = C_id;
+        this.name = C_Name;
+        this.Attack = C_Attack;
+        this.Health = C_Health;
+    }
+} 
