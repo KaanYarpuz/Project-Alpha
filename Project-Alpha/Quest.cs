ce4d9e4c5e27629c8d15173a1f98f18a212c3b4b
rubsn489sg87ga <max.j.p@outlook.com>
2026-02-24 14:24:41 +0100

    quest.cs

--- Project-Alpha/Quest.cs
+++ Project-Alpha/Quest.cs
@@ -2,6 +2,16 @@ namespace Project_Alpha
 {
     public class Quest
     {
+        public int ID; 
+        public string TITLE; 
+        public string TASK; 
         
+        public Quest(int id, string title, string task)
+        {
+            ID = id; 
+            TITLE = title; 
+            TASK = task; 
+            
+        }
     }
 }
