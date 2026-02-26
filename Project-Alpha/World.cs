91d81c4a8ae1b9405ef2b290858bfa2a8483d61a
EmGnome <leonvhr@gmail.com>
2026-02-26 16:40:22 +0100

    Dihlly ohh yollie

--- Project-Alpha/World.cs
+++ Project-Alpha/World.cs
@@ -76,7 +76,7 @@ public static class World
 
     public static void PopulateMonsters()
     {
-        Monster rat = new Monster(MONSTER_ID_RAT, "rat", 1, 3);
+        Monster rat = new Monster(MONSTER_ID_RAT, "devious trio of rats", 3, 9);
 
 
         Monster snake = new Monster(MONSTER_ID_SNAKE, "snake", 10, 7);
